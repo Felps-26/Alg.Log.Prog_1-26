b334119d22652a707e0f0cbed9e9839bee02285f
Felps-26 <filipiramos4152@gmail.com>
2026-04-13 10:14:29 -0300

    .

--- Exercícios/Lista01/01.c
+++ Exercícios/Lista01/01.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    char nome[100];
-    printf("Digite o seu nome: ");
-    scanf("%99s", nome); // Lê o nome digitado
-    printf("Olá, %s! Seja bem-vindo(a).\n", nome);
-    return 0;
+  char nome[100];
+  printf("Digite o seu nome: ");
+  scanf("%99s", nome);
+  printf("Olá, %s! Seja bem-vindo(a).\n", nome);
+  return 0;
 }
